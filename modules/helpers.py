f844f9d2971e460dee5a0264a69eea1c32ac38eb
abhinavxjha <abhinavjha505@gmail.com>
2026-06-22 01:38:08 +0530

    Refactor helper functions for course and skill mgt

--- modules/helpers.py
+++ modules/helpers.py
@@ -11,3 +11,25 @@ def get_required_skills(required_skills_data, selected_career):
             return career['skills']
     return []
 
+def load_courses(filename):
+    with open(filename,"r") as f:
+        courses=json.load(f)
+        return courses
+
+def career_selection():
+    career=input("Which Career You Want To Opt :: ")
+    return career
+
+def course_recommendation(final_missing_skills, courses_data):
+    recommended_courses=[]
+    for course in courses_data:
+        if course['skill'] in final_missing_skills:
+            recommended_courses.append(course)
+    return recommended_courses
+    
+def values(required_skills_data, selected_career, user_skills):
+    required_skills = get_required_skills(required_skills_data, selected_career)
+    missing_skills=list(set(required_skills)-set(user_skills))
+    final_missing_skills=sorted(missing_skills)
+    return final_missing_skills
+    
